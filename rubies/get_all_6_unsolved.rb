7d985744fce56600f3b5ca58bb23130073344ffa
lacemaker <merlettaia@gmail.com>
2013-08-10 22:25:33 +0400

    rewrote solver functions for 3 operators

--- rubies/get_all_6_unsolved.rb
+++ rubies/get_all_6_unsolved.rb
@@ -6,25 +6,37 @@ require "./problem"
 require "./noisy_listener"
 require "./solver_functions"
 
-UNARIES = ["not", "shl1", "shr1", "shr4", "shr16"]
-BINARIES = ["and", "or", "xor", "plus"]
-#h = Net::HTTP.new('icfpc2013.cloudapp.net')
-#resp = h.post('/myproblems?auth=00306ooBUr4BvTjTrWJJyZfHXbwBVU2kLk1cipuAvpsH1H', nil)
 
-f = File.open('metadata.txt') 
-arr = f.gets #resp.body 
-@arr = Problem.array_from_json(arr,6)
+h = Net::HTTP.new('icfpc2013.cloudapp.net')
+resp = h.post('/myproblems?auth=00306ooBUr4BvTjTrWJJyZfHXbwBVU2kLk1cipuAvpsH1H', nil)
+
+#f = File.open('metadata.txt') 
+arr = resp.body 
+sleep(4.1)
+@arr = Problem.array_from_json(arr,6).select{|x| !x.solved }
 
 p @arr.size
 
 @arr.each do |problem|
   if !problem.solved && (problem.time_left.nil? || problem.time_left>0)
-    p "solving #{problem.id}"
-    p "operators: #{problem.operators}  #{problem.size} #{problem.solved} #{problem.time_left}"
+ 
    
-      solve_for_operators_size3(problem) if problem.operators.size == 3
+      
       solve_for_operators_size4(problem) if problem.operators.size == 4
   end
 end
-
-
+problem = @arr[0] if @arr.size > 0
+  p "solving #{problem.id}"
+  p "operators: #{problem.operators}  #{problem.size} #{problem.solved} #{problem.time_left}"
+if problem && problem.operators.size == 3
+  solve_for_operators_size3(problem) do |exp|
+    #result = basic_guess(problem.id, exp)
+    #if result != "200"
+    #  puts 'ERROR ERROR'
+    #  puts problem.id
+    #  break
+    #end
+    #sleep(4.1)
+    p exp
+  end
+end
--- rubies/solver_functions.rb
+++ rubies/solver_functions.rb
@@ -11,11 +11,34 @@ BINARIES = ["and", "or", "xor", "plus"]
 
 def solve_for_operators_size3(problem)
   p "HELLO"
+  c =0
+  #we select how many binary operators in array + 1
+  n = problem.operators.count{|x| BINARIES.include? x }
+  variables = ['x', '0', '1'].repeated_permutation(n + 1)
+  problem.operators.permutation(3).each do |perm|
+    variables.each do |vars|
+      exp = '(' + perm.reverse.join(' (') + ' ' + vars[0]
+        c = c+1
+        var_counter = 1
+        perm.each do |op|
+          if BINARIES.include? op
+            exp = exp + ' ' + vars[var_counter] + ')'
+            var_counter = var_counter + 1
+          else
+            exp = exp + ')'
+          end
+
+        end
+        exp = "(lambda(x) #{exp})"# resulting formula. now correctly works for binary and unary operators
+        yield exp if block_given? #optionally sends result to a block
+       # p exp
+    end   
+  end
+  p "total number of permutations: #{c}"
 end
 
 def solve_for_operators_size4(problem)
   return if problem.solved
-  p "HELLO1"
   problem.operators.permutation(4) do |perm|
     exp = "(lambda (x) (#{perm.join(' (')} x)))))"
     result = basic_guess(problem.id, exp)
